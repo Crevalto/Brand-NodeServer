a98153888dcee0925b81314126a11e26f7b09045
RohitRajP <RohitRajP>
2020-04-28 11:29:50 +0530

    Initial commit - setup skeleton, basic router and packages

---
+++ index.js
@@ -0,0 +1,16 @@
+// importing required packages
+const express = require("express");
+const bodyParser = require("body-parser");
+
+// creating express middleware
+const app = express();
+
+// defining routes
+app.use("/", (req, res, next) => {
+  res.send(
+    "We appreciate your interest. Please wait, development in progress!"
+  );
+});
+
+// starting listener on ports
+app.listen(5000);
