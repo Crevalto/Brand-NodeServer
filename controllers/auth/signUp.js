a16a642b34b3b0de1ba6f54e9f78debf5f1285fb
RohitRajP <RohitRajP>
2020-05-08 23:14:13 +0530

    Stabilized the Sign Up module

--- controllers/auth/signUp.js
+++ controllers/auth/signUp.js
@@ -1,16 +1,24 @@
 // requiring required models
 const User = require("../../models/client/brandUserDetail");
-const express = require("express");
-// const User = require("../models/User");
-const auth = require("../../middleware/auth");
+
+// generates random number for OTP
+const genOTP = () => {
+  while (true) {
+    var x = Math.floor(Math.random() * 9999 + 1000);
+    if (x < 9999 && x > 1000) return x;
+    else continue;
+  }
+};
+
 // creates new user in database
 exports.signUpUser = async (req, res, next) => {
+  // getting request body
   const reqBody = req.body;
-  try {
-    console.log(reqBody);
 
+  try {
+    // creating json document to insert into database
     const userData = {
-      bandName: reqBody["bandName"],
+      brandName: reqBody["brandName"],
       accountPassword: reqBody["accountPassword"],
       brandAddress: reqBody["brandAddress"],
       identificationDetail: {
@@ -19,23 +27,41 @@ exports.signUpUser = async (req, res, next) => {
       },
       emailAddress: reqBody["emailAddress"],
       phoneNo: reqBody["phoneNo"],
+      verifiedUser: false,
       brandAssets: {
         brandLogoSrc: reqBody["brandAssets"]["brandLogoSrc"],
         brandColor: reqBody["brandAssets"]["brandColor"],
         brandSoundTrack: reqBody["brandAssets"]["brandSoundTrack"],
       },
     };
+    // creating user document instance
     const user = new User(userData);
-    const usertoken = await user.generateAuthToken();
+    // // creating token for user and adding it to model
+    // await user.generateAuthToken();
+    // inserting data into database
+    await user.save();
+    // generating OTP code
+    const OTPCode = genOTP();
 
-    const bandName = await user.bandName;
-    res
-      .status(201)
-      .send({ status: true, brandName: bandName, token: usertoken });
-    // res.send({ status: true });
+    // sending response
+    res.status(201).send({ status: true, otp: OTPCode });
   } catch (error) {
-    console.log(error);
+    // creating instance of error message
+    let errorMsg = "";
 
-    res.status(400).send({ error: "Fill out the required fields" });
+    // catching mongoose errors
+    if ((error.name = "MongoError")) {
+      // go through all validation type errors
+      for (var errName in error.errors) {
+        switch (error.errors[errName].kind) {
+          case "required":
+            errorMsg = "Please fill all required fields";
+            break;
+        }
+      }
+      // checking for mongoose error
+      if (error.code === 11000) errorMsg = "User already exists in database";
+    }
+    res.send({ status: false, error: errorMsg });
   }
 };
