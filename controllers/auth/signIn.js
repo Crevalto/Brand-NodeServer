61510a2e1b1d5e2df5d088fbea4ad192b7811546
RohitRajP <RohitRajP>
2020-05-08 23:59:19 +0530

    Stabilized the login module and also added verified user dependant checks and responses

--- controllers/auth/signIn.js
+++ controllers/auth/signIn.js
@@ -1,24 +1,53 @@
+// getting required models
 const User = require("../../models/client/brandUserDetail");
-
-// const User = require("../models/User");
-//const auth = require("../../middleware/auth");
 
 exports.signInUser = async (req, res) => {
   //Login a registered user
   try {
+    // fetching the authentication values from the request body
     const { emailAddress, accountPassword } = req.body;
 
+    // finding user based on the credentials
     const user = await User.findByCredentials(emailAddress, accountPassword);
+    // if user does not exists
     if (!user) {
       return res
         .status(401)
         .send({ status: false, error: "Invalid credentials, please verify" });
     }
-    const token = await user.generateAuthToken();
-
-    const name = await user.bandName;
-
-    res.send({ status: true, name, token });
+    // checking if the user is a verified user
+    if (user.verifiedUser) {
+      // generating auth token for the user
+      const tokenGenerated = await user.generateAuthToken();
+      // updating value in database
+      const newUser = await User.findByIdAndUpdate(
+        user._id,
+        { token: tokenGenerated },
+        { new: true },
+        (err, user) => {
+          if (err)
+            res.status(500).send({
+              status: false,
+              error: "Internal Server Error",
+            });
+          return user;
+        }
+      );
+      // sending response for login
+      res.send({
+        status: true,
+        name: user.brandName,
+        token: user.token,
+        verifiedUser: user.verifiedUser,
+      });
+    } else {
+      // sending response for login
+      res.send({
+        status: false,
+        name: user.brandName,
+        verifiedUser: user.verifiedUser,
+      });
+    }
   } catch (error) {
     console.log(error);
 
