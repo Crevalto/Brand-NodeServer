33679bc9b889d1ae94ff2c6d466d57cd4b95cc6f
RohitRajP <RohitRajP>
2020-05-09 23:36:22 +0530

    Fixed the POST bug for getProducts

--- controllers/vendorProductAPIs/getProducts.js
+++ controllers/vendorProductAPIs/getProducts.js
@@ -1,22 +1,27 @@
+const mongoose = require("mongoose");
+
 // importing the required database model
 const VendorProducts = require("../../models/vendor/vendorProduct");
 
 // function that returns all products of given category
 exports.getProducts = (req, res, next) => {
   // getting the bodyparser parsed body
-  const reqBody = req.body;
+  let categoryId;
+
+  try {
+    categoryId = mongoose.Types.ObjectId(req.body["categoryId"]);
+  } catch (err) {
+    res.send({ status: false, error: "Invalid category Id" });
+  }
 
   // fetching all categories from the database
-  VendorProducts.find({ category: reqBody.categoryId }, function (
-    err,
-    products
-  ) {
+  VendorProducts.find({ category: categoryId }, function (err, products) {
     var prodMap = [];
 
     products.forEach(function (product) {
       prodMap.push(product);
     });
 
-    res.send(prodMap);
+    res.send({ status: true, products: prodMap });
   });
 };
